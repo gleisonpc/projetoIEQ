da3c9038ad38f4eecfe01e8949b9ac383e8e4be0
gleisonpc <gleisonpc@gmail.com>
2014-01-20 22:08:02 -0200

    UP02
    UP02

---
+++ projetoIEQ/projetoIEQ/Controllers/RelatóriosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projetoIEQ.Controllers
+{
+    public class RelatóriosController : Controller
+    {
+        //
+        // GET: /Relatórios/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ projetoIEQ/projetoIEQ/Models/Home.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projetoIEQ.Models
+{
+    public class Home
+    {
+    }
+}
